5421125230bca457c57725509d7e44570ae100ff
lokhacodes <sumaiyasonali421@gmail.com>
2026-02-28 00:04:33 +0600

    useState, Changing State, Update State with call back, toggling state, state-arrays

--- learnings.md
+++ learnings.md
@@ -1,1 +1,19 @@
 ![alt text](image.png)
+![alt text](image-1.png)
+![alt text](image-2.png)
+![alt text](image-3.png)
+
+1. You have 2 options for what you can pass in to a
+   state setter function (e.g. `setCount`). What are they?
+   1. pass the new version of state that we want to use as the replacement for the old version of state.
+   2. Pass a callback function. Must return what we want the new value of state to be. Will receive the  old version of state as a parameter so we can use it to  help determine what we want the new value  of state to be. 
+
+
+2. When would you want to pass the first option (from answer
+   above) to the state setter function?
+Whenver we don't really care about or need the old value, we simply want to set a new value. 
+
+
+3. When would you want to pass the second option (from answer
+   above) to the state setter function?
+Whenver we do care about the previous value in state and need it to help us determine what the new value should be.
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,12 @@
+import React from "react"
 export default function Main () {
-    const ingredients = [
-        "oregano",
+    const [ingredients, setIngredients] = React.useState([
+        /*"oregano",
         "basil",
         "thyme",
         "rosemary",
-        "parsley"
-    ]
+        "parsley"*/
+    ])
 
     const ingredientsListItems = ingredients.map(ingredient =>(
         <li key={ingredient}>{ingredient}</li>
@@ -13,14 +14,16 @@ export default function Main () {
 
     function handleSubmit(event){
         event.preventDefault()
-        console.log("Form submitted")
+       
+        //console.log("Form submitted")
         const formdata = new FormData(event.currentTarget)
         const newIngredient = formdata.get("ingredient")
-        ingredients.push(newIngredient)
-        console.log("Updated ingredients list:", ingredients)
+       // ingredients.push(newIngredient)
+      //  console.log("Updated ingredients list:", ingredients)
+       setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
 
-    
+
     return (
         <main>
             <form className="ingredient-form" onSubmit={handleSubmit}> 
