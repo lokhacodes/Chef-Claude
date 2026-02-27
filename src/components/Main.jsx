614b5047139b4bd81a48e175008a50e72d307f73
lokhacodes <sumaiyasonali421@gmail.com>
2026-02-27 22:13:25 +0600

    Map ingredient list

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,40 @@
 export default function Main () {
+    const ingredients = [
+        "oregano",
+        "basil",
+        "thyme",
+        "rosemary",
+        "parsley"
+    ]
 
+    const ingredientsListItems = ingredients.map(ingredient =>(
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function handleSubmit(event){
+        event.preventDefault()
+        console.log("Form submitted")
+        const formdata = new FormData(event.currentTarget)
+        const newIngredient = formdata.get("ingredient")
+        ingredients.push(newIngredient)
+        console.log("Updated ingredients list:", ingredients)
+    }
+
+    
     return (
         <main>
-            <form className="ingredient-form"> 
+            <form className="ingredient-form" onSubmit={handleSubmit}> 
               <input 
               type= "text"
               placeholder="e.g. oregano"
               aria-label="Add ingredient"
+              name="ingredient"
               />
               <button type="submit">Add Ingredient </button>  
             </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
         </main>
     )
 
